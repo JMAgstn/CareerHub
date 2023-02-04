ec08418a788fc58411b65b4d76d85a2f052f9714
JMAgstn <119966783+JMAgstn@users.noreply.github.com>
2023-02-05 01:29:15 +0800

    create working before adding message

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
@@ -14,4 +15,25 @@ class ListingController extends Controller
    public function show(Listing $listing) {
         return view('listings.show', ['listing' => $listing]);
    }
+
+   public function create() {
+        return view('listings.create');
+   }
+
+   public function store(Request $request){
+    $formFields = $request->validate([
+        "company" => ['required', Rule::unique('listings')],
+        "title" => 'required',
+        "location" => 'required',
+        "email" => ['required', 'email'],
+        "website" => 'required',
+        "tags" => 'required',
+        "description" => 'required',
+   ]);
+
+    Listing::create($formFields);
+
+    return redirect('/')->with('message', 'New Student was added successfully!');
+    }
 }
+
